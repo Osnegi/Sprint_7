c91897c6a742dddb91b9a3a14aeb4617a835e27b
Olga Snegireva <osnegireva@gmail.com>
2024-09-11 21:36:18 +0100

    third commit with allure results

---
+++ Tests/urls.py
@@ -0,0 +1,10 @@
+BASE_URL = "https://qa-scooter.praktikum-services.ru/api/v1"
+
+REG_COURIER_ENDPOINT = "/courier"
+
+LOGIN_COURIER_ENDPOINT = "/courier/login"
+
+CREATE_ORDER_ENDPOINT = "/orders"
+
+GET_LIST_ORDERS_ENDPOINT = "/orders"
+
--- samokat_api.py
+++ samokat_api.py
@@ -1,25 +1,20 @@
 import requests
 import urls
-import allure
 
 class SamokatApi:
 
-    @allure.step('Зарегистрировать нового курьера')
     @staticmethod
     def registration_courier(body):
         return requests.post(urls.BASE_URL + urls.REG_COURIER_ENDPOINT, data=body)
 
-    @allure.step('Авторизовать курьера')
     @staticmethod
     def login_courier(body):
         return requests.post(urls.BASE_URL + urls.LOGIN_COURIER_ENDPOINT, data=body)
 
-    @allure.step('Создать новый заказ')
     @staticmethod
     def create_order(body):
         return requests.post(urls.BASE_URL + urls.CREATE_ORDER_ENDPOINT, json=body)
 
-    @allure.step('Получить список всех заказов')
     @staticmethod
     def get_list_orders():
         return requests.get(urls.BASE_URL + urls.GET_LIST_ORDERS_ENDPOINT)
